1c8acb97b99daccfbc6dc00cafd77010c56c7be4
vanshdeep singh <vanshdeep00220022@gmail.com>
2026-07-28 02:33:40 +0530

    Add files via upload

---
+++ 🫆variable-vault/basics.js
@@ -0,0 +1,22 @@
+const accountID = 144556
+let accountEmail = "vansh456@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+// accountID = 2 // NOT ALLOWED......
+
+accountEmail = "c@hc.com"
+accountPassword = "284280"
+accountCITY = "delhi"
+
+//console.log(accountId);
+
+
+
+/*
+PREFER NOT TO USE BECUASE OF ISSUE IN BLOCK SCOPE AND FUNCTIONAL SCOPE
+*/
+
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
